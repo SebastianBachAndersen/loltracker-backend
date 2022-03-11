3508330c2cc793259403294cda9ccae2b80fa8b4
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-11 12:35:32 +0100

    Added summoners table

--- app/Http/Controllers/SummonerController.php
+++ app/Http/Controllers/SummonerController.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Log;
 class SummonerController extends Controller
 {
     public function index(string $summonerName) {
-        $response = Http::withHeaders(['X-Riot-Token' => env('RIOT_GAMES_API_KEY')])->get(env('RIOT_GAMES_API_URL') . "/lol/summoner/v4/summoners/by-name/$summonerName");
+        $response = Http::withHeaders(['X-Riot-Token' => env('RIOT_GAMES_API_KEY')])
+            ->get(env('RIOT_GAMES_API_URL') . "/lol/summoner/v4/summoners/by-name/$summonerName");
 
         Log::info($response);
     }
---
+++ database/migrations/2022_03_11_105250_add_summoners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('summoners', function (Blueprint $table) {
+            $table->id();
+            $table->string('summonerId');
+            $table->string('puuid');
+            $table->string('name');
+            $table->integer('profileIconId');
+            $table->dateTime('revisionDate');
+            $table->integer('summonerLevel');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('summoners');
+    }
+};
