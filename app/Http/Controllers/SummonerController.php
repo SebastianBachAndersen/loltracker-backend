4ea35e9d434711fb04614d8116d406686b6fd9e7
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-11 11:51:03 +0100

    Added summoner endpoint

---
+++ app/Http/Controllers/SummonerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class SummonerController extends Controller
+{
+    public function index(string $summonerName) {
+        $response = Http::withHeaders(['X-Riot-Token' => env('RIOT_GAMES_API_KEY')])->get(env('RIOT_GAMES_API_URL') . "/lol/summoner/v4/summoners/by-name/$summonerName");
+
+        Log::info($response);
+    }
+}
