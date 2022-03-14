0e2de552fd520dda86b012c6d4e7c45d5d2b0d61
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-14 09:45:16 +0100

    added region support

--- app/Http/Controllers/SummonerController.php
+++ app/Http/Controllers/SummonerController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SummonerResource;
 use App\Models\Summoner;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class SummonerController extends Controller
 {
-    public function index(string $summonerName) {
-        $response = Http::withHeaders(['X-Riot-Token' => env('RIOT_GAMES_API_KEY')])
-            ->get(env('RIOT_GAMES_API_URL') . "/lol/summoner/v4/summoners/by-name/$summonerName");
+    public function index(string $region, string $summonerName) {
+        $response = Http::withHeaders(['X-Riot-Token' => config("services.riot_games_api.api_key")])
+            ->get("https://" . config("services.riot_games_api.regions.$region") .  config("services.riot_games_api.url") . "/lol/summoner/v4/summoners/by-name/$summonerName");
 
-        Log::info($response);
-//"id":"nzDvmF-muHztmm9ymcEcemtlwJZb22lXg-Nmaffs9U6LZVA","accountId":"xyl36KBxZKmgviHTvevqI77puxvDvP5Ot0_jtmp4lAuFuw","puuid":"skRlolfMPNAa_0CKluimlBt55xp33XdpxYYSlf2jZE0HtM4Hsaj5HbqXhtZSGXp2Vg5aABGJetdN3A","name":"Sex med mænd","profileIconId":505,"revisionDate":1646833798739,"summonerLevel":281}
-        $summoner = null;
         if ($response->successful()) {
-            Log::info("success");
-            if (!Summoner::where('summonerId', $response['id'])->first()) {
+            $summoner = Summoner::where('summonerId', $response['id'])->first();
+            if (!$summoner) {
                 $summoner = Summoner::create([
                     'summonerId' => $response['id'],
                     'accountId' => $response['accountId'],
@@ -30,9 +26,8 @@ class SummonerController extends Controller
                     'summonerLevel' => $response['summonerLevel']
 
                 ]);
-                Log::info($summoner);
             }
-            return response(["response" => "success"], 200);
+            return (new SummonerResource($summoner))->additional(['result' => 'success']);
         } else {
             return response(['error' => 'not found'], 404);
         }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('summoner/{summonerName}', [SummonerController::class, 'index']);
+Route::get('summoner/{region}/{summonerName}', [SummonerController::class, 'index']);
