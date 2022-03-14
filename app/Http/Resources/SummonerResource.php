3b7afb88c62aae495d67723547ec774fcc09b61c
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-14 09:01:24 +0100

    rename

--- app/Http/Resources/SummonerResource.php
+++ app/Http/Resources/SummonerResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class summoner extends JsonResource
+class SummonerResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
