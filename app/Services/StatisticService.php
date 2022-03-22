4ba57cbcaec98947e1dec6da7796ce242e69588e
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-22 14:15:01 +0100

    Add calculation of champion stats for game

--- app/Services/StatisticService.php
+++ app/Services/StatisticService.php
@@ -10,9 +10,9 @@ class StatisticService
 {
 
     public static function saveChampionStatisticsForGame($match) {
-            DB::beginTransaction( function () use ($match) {
+            DB::transaction( function () use ($match) {
                 foreach ($match['info']['participants'] as $participant) {
-                    SummonerChampionStat::create([
+                    $champStats = SummonerChampionStat::create([
                         'summonerId' => $participant['summonerId'],
                         'championId' => $participant['championId'],
                         'matchId' => $match['metadata']['matchId'],
@@ -29,6 +29,33 @@ class StatisticService
                         'pentaKills' => $participant['pentaKills'],
                         'season'
                     ]);
+                    if ($sumStats = SummonerChampionStatsSummary::where('championId', $participant['championId'])->where('summonerId',$participant['summonerId'])->first()) {
+                            $sumStats->wins += ($champStats->win ? 1 : 0);
+                            $sumStats->losses += (!$champStats->win ? 1 : 0);
+                            $sumStats->kills += $champStats->kills;
+                            $sumStats->deaths += $champStats->deaths;
+                            $sumStats->assits += $champStats->assits;
+                            $sumStats->cs += $champStats->cs;
+                            $sumStats->damage += $champStats->damage;
+                            $sumStats->gold += $champStats->gold;
+                            $sumStats->doubleKills += $champStats->doubleKills;
+                            $sumStats->tripleKills += $champStats->tripleKills;
+                            $sumStats->quadraKills += $champStats->quadKills;
+                            $sumStats->pentaKills += $champStats->pentaKills;
+                            $sumStats->kda = ($sumStats->kills + $sumStats->assits) ?? 1 / $sumStats->deaths ?? 1;
+                            $sumStats->winRate = ($sumStats->wins / ($sumStats->wins + $sumStats->losses)) * 100;
+                            $sumStats->save();
+                            return;
+                    }
+
+                    SummonerChampionStatsSummary::create([
+                        ...$champStats->toArray(),
+                        'wins' => ($champStats->win ? 1 : 0),
+                        'losses' => (!$champStats->win ? 1 : 0),
+                        'kda' => ($champStats->kills + $champStats->assits) ?? 1 / $champStats->deaths ?? 1,
+                        'winRate' => $champStats->win / ($champStats->win + !$champStats->win) * 100,
+                    ]);
+
                 }
             });
 
@@ -39,26 +66,26 @@ class StatisticService
         $championStatistics = SummonerChampionStat::where('summonerId', $summonerId)->where('championId', $championId)->get();
 
         if ($championStatistics->count() > 0) {
-            $summonerChampionStatistics = new SummonerChampionStatsSummary();
-            $summonerChampionStatistics->summonerId = $summonerId;
-            $summonerChampionStatistics->championId = $championId;
+            $sumStats = new SummonerChampionStatsSummary();
+            $sumStats->summonerId = $summonerId;
+            $sumStats->championId = $championId;
             foreach ($championStatistics as $championStatistic) {
-                $summonerChampionStatistics->wins += ($championStatistic->win ? 1 : 0);
-                $summonerChampionStatistics->losses += (!$championStatistic->win ? 1 : 0);
-                $summonerChampionStatistics->kills += $championStatistic->kills;
-                $summonerChampionStatistics->deaths += $championStatistic->deaths;
-                $summonerChampionStatistics->assits += $championStatistic->assits;
-                $summonerChampionStatistics->cs += $championStatistic->cs;
-                $summonerChampionStatistics->damage += $championStatistic->damage;
-                $summonerChampionStatistics->gold += $championStatistic->gold;
-                $summonerChampionStatistics->doubleKills += $championStatistic->doubleKills;
-                $summonerChampionStatistics->tripleKills += $championStatistic->tripleKills;
-                $summonerChampionStatistics->quadraKills += $championStatistic->quadKills;
-                $summonerChampionStatistics->pentaKills += $championStatistic->pentaKills;
+                $sumStats->wins += ($championStatistic->win ? 1 : 0);
+                $sumStats->losses += (!$championStatistic->win ? 1 : 0);
+                $sumStats->kills += $championStatistic->kills;
+                $sumStats->deaths += $championStatistic->deaths;
+                $sumStats->assits += $championStatistic->assits;
+                $sumStats->cs += $championStatistic->cs;
+                $sumStats->damage += $championStatistic->damage;
+                $sumStats->gold += $championStatistic->gold;
+                $sumStats->doubleKills += $championStatistic->doubleKills;
+                $sumStats->tripleKills += $championStatistic->tripleKills;
+                $sumStats->quadraKills += $championStatistic->quadKills;
+                $sumStats->pentaKills += $championStatistic->pentaKills;
             }
-            $summonerChampionStatistics->kda = ($summonerChampionStatistics->kills + $summonerChampionStatistics->assits) / $summonerChampionStatistics->deaths;
-            $summonerChampionStatistics->winRate = ($summonerChampionStatistics->wins / $championStatistics->count()) * 100;
-            return SummonerChampionStatsSummary::create($summonerChampionStatistics->toArray());
+            $sumStats->kda = ($sumStats->kills + $sumStats->assits) ?? 1 / $sumStats->deaths ?? 1;
+            $sumStats->winRate = ($sumStats->wins / $championStatistics->count()) * 100;
+            return SummonerChampionStatsSummary::create($sumStats->toArray());
         } else {
             return null;
         }
